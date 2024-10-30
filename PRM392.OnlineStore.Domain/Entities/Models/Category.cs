658d18412d594b7d43f711c36a2bde47dccd5b27
sonhohuu <hhson365@gmail.com>
2024-10-30 08:34:58 +0700

    fix(db): add identity for entity

--- PRM392.OnlineStore.Domain/Entities/Models/Category.cs
+++ PRM392.OnlineStore.Domain/Entities/Models/Category.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace PRM392.OnlineStore.Domain.Entities.Models;
 
 public partial class Category
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Key]
     public int CategoryId { get; set; }
 
     public string CategoryName { get; set; } = null!;
